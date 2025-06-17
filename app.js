7995dd43bbf7f034b90d318f310a4138ee7a4b8c
MEHARBRAND <alihaider20101612@gmail.com>
2025-06-17 13:07:59 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const API_URL = 'https://script.google.com/macros/s/AKfycbybk_yUJEXrW91Y8YRfHUTg8Xuy43TGIQjZ8DBYhaGC4bhfAOLjsW_cNqRTaJhZxbQwOA/exec';
 
+// Show apps
 fetch(API_URL)
   .then(res => res.json())
   .then(apps => {
@@ -9,13 +10,13 @@ fetch(API_URL)
       div.innerHTML = `
         <h3>${app.name}</h3>
         <p>${app.description}</p>
-        <img src="${app.iconUrl}" width="100"><br>
         <a href="https://drive.google.com/uc?export=download&id=${app.apkDriveId}">Download APK</a>
       `;
       container.appendChild(div);
     });
   });
 
+// Submit form
 document.getElementById('submit-form').addEventListener('submit', e => {
   e.preventDefault();
   const form = e.target;
@@ -25,11 +26,12 @@ document.getElementById('submit-form').addEventListener('submit', e => {
     iconUrl: form.iconUrl.value,
     apkDriveId: form.apkDriveId.value
   };
+
   fetch(API_URL, {
     method: 'POST',
     body: JSON.stringify(data)
   })
-  .then(res => res.json())
-  .then(res => alert('Submitted! Your app ID: ' + res.id))
-  .catch(err => alert('Error: ' + err));
+    .then(res => res.json())
+    .then(res => alert('Submitted! Your app ID: ' + res.id))
+    .catch(err => alert('Error: ' + err));
 });
